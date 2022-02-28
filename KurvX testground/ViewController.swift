ce33e0d77456e875937ec24b6951d1036d6365f3
Quang Trung <qttran@x-log.de>
2022-02-28 15:15:52 +0100

    Add new project

---
+++ KurvX testground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KurvX testground
+//
+//  Created by Trung Tran on 28.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KurvX testgroundUITests/KurvX_testgroundUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KurvX_testgroundUITestsLaunchTests.swift
+//  KurvX testgroundUITests
+//
+//  Created by Trung Tran on 28.02.22.
+//
+
+import XCTest
+
+class KurvX_testgroundUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
