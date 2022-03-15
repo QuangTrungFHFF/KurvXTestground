b651eeca64237eef88a500ae217ac7475ad265e0
Quang Trung <qttran@x-log.de>
2022-03-15 12:57:05 +0100

    Add new storyboard and assets

--- KurvX testground/Controllers/MainViewController.swift
+++ KurvX testground/Controllers/MainViewController.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var navigationButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("Navigation button" + sender.currentTitle!)
+        
+    }
+    
 }
 
