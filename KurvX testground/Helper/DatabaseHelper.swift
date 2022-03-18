04fdb453d1f1e25c7969cf72369958dab0910d1d
Quang Trung <qttran@x-log.de>
2022-03-18 10:54:41 +0100

    Add database helper and create table statement

--- KurvX testground/Helper/DatabaseHelper.swift
+++ KurvX testground/Helper/DatabaseHelper.swift
@@ -7,3 +7,16 @@
 
 import Foundation
 
+public class DatabaseHelper
+{
+    static let TEXT_TYPE = " TEXT"
+    static let BOOLEAN_TYPE = " INTEGER"
+    static let INTEGER_TYPE = " INTEGER"
+    static let FLOAT_TYPE = " REAL"
+    static let NOT_NULL = " NOT NULL"
+    static let UNIQUE = " UNIQUE"
+    static let COMMA_SEP = ","
+    static let _ID = "_id"
+    static let _COUNT = "_count"
+
+}
--- KurvX testground/KurvxManager/KurvxDatabaseManager.swift
+++ KurvX testground/KurvxManager/KurvxDatabaseManager.swift
@@ -15,6 +15,33 @@ public class KurvxDatabaseManager : NSObject{
     private var peripheralsDB: OpaquePointer?
     private let peripheralsDBPath: String = "kurvXDb.sqlite"
     
+    let createTableString = """
+    CREATE TABLE \(KurvxDbTable.TABLE_NAME)(
+    \(KurvxDbTable.COLUMN_UUID_NUMBER)\(DatabaseHelper.INTEGER_TYPE) PRIMARY KEY AUTOINCREMENT,
+    \(KurvxDbTable.COLUMN_UUID_NUMBER)\(DatabaseHelper.TEXT_TYPE)\(DatabaseHelper.NOT_NULL)\(DatabaseHelper.UNIQUE),
+    \(KurvxDbTable.COLUMN_SERIAL_NUMBER)\(DatabaseHelper.TEXT_TYPE)\(DatabaseHelper.NOT_NULL)\(DatabaseHelper.UNIQUE),
+    \(KurvxDbTable.COLUMN_DEVICE_NAME)\(DatabaseHelper.TEXT_TYPE),
+    \(KurvxDbTable.COLUMN_LAST_SELECTED)\(DatabaseHelper.BOOLEAN_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A00)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A01)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A02)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A10)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A11)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A12)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A20)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A21)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_A22)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_PITCH)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_ROLL)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_YAW)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_MEAN_UP_X)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_MEAN_UP_Y)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_MEAN_UP_Z)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_MEAN_LEAN_X)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_MEAN_LEAN_Y)\(DatabaseHelper.FLOAT_TYPE),
+    \(KurvxDbTable.COLUMN_DEVICE_ROT_MAT_MEAN_LEAN_Z)\(DatabaseHelper.FLOAT_TYPE));
+    """
+    
     private override init(){
         super.init()
         peripheralsDB = self.openDatabase()
